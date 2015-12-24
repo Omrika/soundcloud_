34502bc24ed5c4059eb9bc46f776c34bb790bd38
Omrika <omrikafolkes@gmail.com>
2015-12-24 12:51:43 -0500

    simplier way to show widget

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $('#submit').click(function(){
-        var trackUrl = $('#text').val();
-        var Client_ID = YOUR_CLIENT_ID;
-        $.get(
-            'http://api.soundcloud.com/resolve.json?url=' + trackUrl + '&client_id=' + Client_ID, 
-            function (result) {//returns json, we only need id in this case
-                $("#player").html('<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/' + result.id +'&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>');//the iframe is copied from soundcloud embed codes
-            }
-        );
+        // var trackUrl = $('#text').val();
+        // var Client_ID = YOUR_CLIENT_ID;
+        // $.get(
+        //     'http://api.soundcloud.com/resolve.json?url=' + trackUrl + '&client_id=' + Client_ID, 
+        //     function (result) {//returns json, we only need id in this case
+        //         $("#player").html('<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/' + result.id +'&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>');//the iframe is copied from soundcloud embed codes
+        //     }
+        // );
+        SC.oEmbed($('#text').val(), function(playlist){
+            $('#player').html(playlist.html)
+        });
     })
 });
